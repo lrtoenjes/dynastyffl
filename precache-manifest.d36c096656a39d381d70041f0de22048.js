ea237e0fd055a44a40e50f91433028e03914a4bf
Luke Toenjes <luket@graham.ca>
2019-06-11 13:09:42 -0600

    Updates

---
+++ precache-manifest.d36c096656a39d381d70041f0de22048.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed37e8115f1d3bc4c76e0f48399c7cf6",
+    "url": "/dynastyffl/index.html"
+  },
+  {
+    "revision": "79c91e6d7d3988d8fba5",
+    "url": "/dynastyffl/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/dynastyffl/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "79c91e6d7d3988d8fba5",
+    "url": "/dynastyffl/static/js/main.8200619f.chunk.js"
+  },
+  {
+    "revision": "6ecfa086c608ddfa6b13",
+    "url": "/dynastyffl/static/js/runtime~main.a40ea988.js"
+  }
+]);
